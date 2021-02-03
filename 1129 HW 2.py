1ede3a3ae7154ee10b7c7d36ad326268617d68dc
mokeke1 <77366291+mokeke1@users.noreply.github.com>
2021-02-03 10:42:47 -0500

    Add files via upload

---
+++ 1129 HW 2.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+Colors= ["red", "blue","orange","brown"]
+for x in Colors:
+    print(x)
+
+
+# In[2]:
+
+
+print(Colors[0])
+
+
+# In[3]:
+
+
+print(Colors[1])
+
+
+# In[4]:
+
+
+print(Colors[2])
+
+
+# In[5]:
+
+
+print(Colors[3])
+
+
+# In[2]:
+
+
+number = [0]
+for n in number:
+    if n < 10:
+        print(n)
+
+
+# In[3]:
+
+
+k = 0
+while k < 10:
+    print(k)
+    k += 1
+
+
+# In[1]:
+
+
+digits = 12
+while digits <= 10:
+    print(digits)
+else:
+    print("greater than 10")
+
+
+# In[6]:
+
+
+v = 30
+sum = 0
+m = 1
+while m <= v:
+    sum = sum + m
+    m = m+1
+print(sum)
+
+
+# In[ ]:
+
+
+
+
